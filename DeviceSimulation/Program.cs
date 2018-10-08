53ed0fc0a572c9a00d3a95cb0435454c86ed2796
AndyHsu <andy.hsu.job@gmail.com>
2018-10-08 11:37:07 +0800

    Project initialization - added mvc & console app

---
+++ DeviceSimulation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeviceSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ IoTPlatform/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IoTPlatform.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
