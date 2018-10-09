79b96f69a3741e787df0d003aecc6177e59cea78
AndyHsu <andy.hsu.job@gmail.com>
2018-10-09 13:21:43 +0800

    Added IoTPlatformOptions.cs

---
+++ DeviceSimulation/Clients/Options/IoTPlatformOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace DeviceSimulation.Clients.Options
+{
+    public class IoTPlatformOptions
+    {
+        public string BaseAddress { get; set; }
+        
+        public string RelativeSendingDataUrl { get; set; }
+    }
+}
