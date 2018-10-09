2c2af4a3bf174eeec6e811c93fee5277a5b59139
AndyHsu <andy.hsu.job@gmail.com>
2018-10-09 13:37:39 +0800

    Added ConveyorSimulatorOptions.cs

---
+++ DeviceSimulation/Simulators/Options/ConveyorSimulatorOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace DeviceSimulation.Simulators.Options
+{
+    public class ConveyorSimulatorOptions
+    {
+        public int SpeedMin { get; set; }
+
+        public int SpeedMax { get; set; }
+
+        public int MaximumItemsPerSecond { get; set; }
+    }
+}
