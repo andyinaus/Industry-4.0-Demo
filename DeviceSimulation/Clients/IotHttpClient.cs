62d736c4ab0d2ee2932d472232b136ff1ae2debf
AndyHsu <andy.hsu.job@gmail.com>
2018-10-09 13:24:10 +0800

    Added IotHttpClient.cs

---
+++ DeviceSimulation/Clients/IotHttpClient.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using DeviceSimulation.Clients.Options;
+using DeviceSimulation.Simulators;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+
+namespace DeviceSimulation.Clients
+{
+    public class IoTHttpClient : IDisposable
+    {
+        private readonly ILogger _logger;
+        private readonly HttpClient _client;
+
+        public IoTHttpClient(IOptions<IoTPlatformOptions> ioTOptions, IOptions<HttpClientOptions> httpOptions, ILogger<IoTHttpClient> logger)
+        {
+            HttpOptions = httpOptions?.Value ?? throw new ArgumentNullException(nameof(httpOptions));
+            IoTOptions = ioTOptions?.Value ?? throw new ArgumentNullException(nameof(ioTOptions));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+            _client = new HttpClient
+            {
+                BaseAddress = new Uri(IoTOptions.BaseAddress)
+            };
+        }
+        
+        public HttpClientOptions HttpOptions { get; private set; }
+
+        public IoTPlatformOptions IoTOptions { get; private set; }
+
+        public async Task<HttpResponseMessage> SendSimulatedDeviceDataAsync(ConveyorSimulator simulator)
+        {
+            HttpResponseMessage response = null;
+
+            var content = new StringContent(JsonConvert.SerializeObject(simulator), Encoding.UTF8,
+                "application/json");
+
+            for (var i = 1; i <= HttpOptions.NumberOfRetries; i++)
+            {
+                try
+                {
+                    response = await _client.PutAsync(IoTOptions.RelativeSendingDataUrl, content);
+
+                    response.EnsureSuccessStatusCode();
+                    Console.WriteLine($"Successfully sent for device {simulator.SerialNumber} with speed {simulator.Speed}.");
+                    break;
+                }
+                catch (HttpRequestException exception)
+                {
+                    if (i == HttpOptions.NumberOfRetries)
+                    {
+                        _logger.LogWarning($"Error at Device {simulator.SerialNumber}: {exception.Message}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{HttpOptions.NumberOfRetries - i} Attempts remaining ...");
+                    }
+                }
+            }
+
+            return response;
+        }
+
+        public void Dispose()
+        {
+            _client?.Dispose();
+        }
+    }
+}
