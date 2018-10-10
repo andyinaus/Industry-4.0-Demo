ba6dd6b863e17a7a24281d13f656c95a55bfd721
AndyHsu <andy.hsu.job@gmail.com>
2018-10-10 20:04:43 +0800

    Added IDeviceReadingRepository.cs

---
+++ IoTPlatform/Repositories/IDeviceReadingRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using IoTPlatform.Persistences;
+
+namespace IoTPlatform.Repositories
+{
+    public interface IDeviceReadingRepository
+    {
+        Task AddAsync(DeviceReading reading);
+        Task<IEnumerable<DeviceReading>> GetAllReadingsByIdAsync(string id);
+    }
+}
