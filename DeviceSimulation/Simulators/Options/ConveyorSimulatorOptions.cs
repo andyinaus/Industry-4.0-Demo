6b5686ea10bf5f0b57e4fc69dca59a8ea1c2c984
AndyHsu <andy.hsu.job@gmail.com>
2018-10-10 22:46:55 +0800

    Changed properties of ConveyorSimulatorOptions

--- DeviceSimulation/Simulators/Options/ConveyorSimulatorOptions.cs
+++ DeviceSimulation/Simulators/Options/ConveyorSimulatorOptions.cs
@@ -1,11 +1,25 @@
-﻿namespace DeviceSimulation.Simulators.Options
+﻿using System.Collections.Generic;
+
+namespace DeviceSimulation.Simulators.Options
 {
     public class ConveyorSimulatorOptions
     {
-        public int SpeedMin { get; set; }
+        public IEnumerable<Simulator> Simulators { get; set; }
+        
+        public SimulatorSettings Settings { get; set; }
 
-        public int SpeedMax { get; set; }
+        public class Simulator
+        {
+            public string DeviceId { get; set; }
+        }
 
-        public int MaximumItemsPerSecond { get; set; }
+        public class SimulatorSettings
+        {
+            public int SpeedMin { get; set; }
+
+            public int SpeedMax { get; set; }
+
+            public int MaximumItemsPerSecond { get; set; }
+        }
     }
 }
