347f26016540ad6a437b1d4a11c2b42a2f82b7b1
AndyHsu <andy.hsu.job@gmail.com>
2018-10-10 22:47:12 +0800

    Fixed broken ConveyorSimulator.cs

--- DeviceSimulation/Simulators/ConveyorSimulator.cs
+++ DeviceSimulation/Simulators/ConveyorSimulator.cs
@@ -12,7 +12,8 @@ namespace DeviceSimulation.Simulators
         public ConveyorSimulator(IClock clock, IOptions<ConveyorSimulatorOptions> options)
         {
             if (clock == null) throw new ArgumentNullException(nameof(clock));
-            if (options == null) throw new ArgumentNullException(nameof(options));
+            if (options?.Value == null) throw new ArgumentNullException(nameof(options));
+            if (options.Value?.Settings == null) throw new ArgumentNullException(nameof(options.Value.Settings));
 
             SerialNumber = Guid.NewGuid();
             DateTime = clock.Now();
@@ -22,10 +23,10 @@ namespace DeviceSimulation.Simulators
         public ConveyorSimulator Simulate()
         {
             var random = new Random();
-            Speed = random.Next(_options.SpeedMin, _options.SpeedMax);
+            Speed = random.Next(_options.Settings.SpeedMin, _options.Settings.SpeedMax);
             PackageTrackingAlarmState = (PackageTrackingAlarmState) random.Next(0, 1);
-            CurrentRecipeCount += random.Next(_options.MaximumItemsPerSecond);
-            CurrentTotalBoards += random.Next(_options.MaximumItemsPerSecond);
+            CurrentRecipeCount += random.Next(_options.Settings.MaximumItemsPerSecond);
+            CurrentTotalBoards += random.Next(_options.Settings.MaximumItemsPerSecond);
             DateTime = DateTime.AddSeconds(1);
 
             return this;
