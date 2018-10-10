b45d29bb124a91c7fdfeafe0901721e8a3421a8a
AndyHsu <andy.hsu.job@gmail.com>
2018-10-10 20:15:33 +0800

    Added DeviceReadingRepository.cs

---
+++ IoTPlatform/Repositories/DeviceReadingRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dapper;
+using IoTPlatform.Infrastructures;
+using IoTPlatform.Persistences;
+using Serilog;
+
+namespace IoTPlatform.Repositories
+{
+    public class DeviceReadingRepository : IDeviceReadingRepository
+    {
+        private readonly IConnectionFactory _connectionFactory;
+
+        private const string TableName = "DeviceReadings";
+
+        public DeviceReadingRepository(IConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory ?? throw new ArgumentNullException(nameof(connectionFactory));
+        }
+
+
+        public async Task AddAsync(DeviceReading reading)
+        {
+            if (reading == null) throw new ArgumentNullException(nameof(reading));
+
+            using (var connection = await _connectionFactory.CreateConnection())
+            {
+                var sql = $"INSERT INTO {TableName} (DeviceId, DateTime, Speed, PackageTrackingAlarmState, CurrentTotalBoards, CurrentRecipeCount)"
+                          + " VALUES(@DeviceId, @DateTime, @Speed, @PackageTrackingAlarmState, @CurrentTotalBoards, @CurrentRecipeCount)";
+
+                await connection.ExecuteAsync(sql, reading);
+
+                Log.Information($"A deviceReading with ID '{reading.DeviceId}' added.");
+            }
+        }
+
+        public async Task<IEnumerable<DeviceReading>> GetAllReadingsByIdAsync(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id)) throw new ArgumentNullException(nameof(id));
+
+            using (var connection = await _connectionFactory.CreateConnection())
+            {
+                var sql = $"SELECT * FROM {TableName}"
+                          + " WHERE DeviceId = @Id";
+
+                return await connection.QueryAsync<DeviceReading>(sql, new { Id = id });
+            }
+        }
+    }
+}
