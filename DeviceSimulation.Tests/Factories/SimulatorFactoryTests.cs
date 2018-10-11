589c4e4845fa4c42819a26d063501dbe2db75105
AndyHsu <andy.hsu.job@gmail.com>
2018-10-11 12:45:43 +0800

    Added SimulatorFactoryTests.cs

---
+++ DeviceSimulation.Tests/Factories/SimulatorFactoryTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using DeviceSimulation.Factories;
+using DeviceSimulation.Simulators.Options;
+using DeviceSimulation.Utils;
+using Microsoft.Extensions.Options;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DeviceSimulation.Tests.Factories
+{
+    [TestClass]
+    public class SimulatorFactoryTests
+    {
+        [TestMethod]
+        public void CtorWhenClockIsNullShouldThrowArgumentNullException()
+        {
+            Action target = () => new SimulatorFactory(CreateDummySimulatorSettingsOptions(), null);
+
+            Assert.ThrowsException<ArgumentNullException>(target);
+        }
+
+        [TestMethod]
+        public void CtorWhenSimulatorSettingsOptionsIsNullShouldThrowArgumentNullException()
+        {
+            Action target = () => new SimulatorFactory(null, new Clock(DateTime.Now));
+
+            Assert.ThrowsException<ArgumentNullException>(target);
+        }
+
+        [TestMethod]
+        public void CreateSimulatorWhenIdIsNullShouldThrowArgumentNullException()
+        {
+            var factory = new SimulatorFactory(CreateDummySimulatorSettingsOptions(), new Clock(DateTime.Now));
+
+            Action target = () => factory.CreateSimulator(null);
+
+            Assert.ThrowsException<ArgumentNullException>(target);
+        }
+
+        [TestMethod]
+        public void CreateSimulatorWhenIdIsWhiteSpaceShouldThrowArgumentNullException()
+        {
+            var factory = new SimulatorFactory(CreateDummySimulatorSettingsOptions(), new Clock(DateTime.Now));
+
+            Action target = () => factory.CreateSimulator(" ");
+
+            Assert.ThrowsException<ArgumentNullException>(target);
+        }
+
+        [TestMethod]
+        public void CreateSimulatorWhenIdIsValidShouldReturnSimulatorWithExpectedId()
+        {
+            var factory = new SimulatorFactory(CreateDummySimulatorSettingsOptions(), new Clock(DateTime.Now));
+
+            const string deviceId = "123456";
+
+            var simulator = factory.CreateSimulator(deviceId);
+
+            Assert.AreEqual(deviceId, simulator.Id);
+        }
+
+        private static IOptions<SimulatorSettingsOptions> CreateDummySimulatorSettingsOptions()
+        {
+            return Options.Create(new SimulatorSettingsOptions());
+        }
+    }
+}
