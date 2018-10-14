30ae59c0fb4d53b286be51cf0e8d2842bf54e462
AndyHsu <andy.hsu.job@gmail.com>
2018-10-14 22:02:09 +0800

    Added real time bar chart and line chart using d3

--- IoTPlatform/wwwroot/js/site.js
+++ IoTPlatform/wwwroot/js/site.js
@@ -4,12 +4,22 @@ if (document.getElementById("Monitor")) {
         el: '#Monitor',
         data: {
             latestReadings: [],
-            speedReadings: [],
-            deviceReadings: []
+            transitionSettings: {
+                duration: 1000,
+                ease: d3.easeLinear
+            },
+            speedTrendSvg: {
+                dict: {},
+                value: null
+            },
+            totalBoardSvg: {
+                value: null
+            },
+            legend: {
+                value: null
+            }
         },
         mounted: function () {
-            this.fetchDeviceReadings();
-            
             setInterval(function () {
                 this.fetchDeviceReadings();
             }.bind(this), 1000);
@@ -20,74 +30,70 @@ if (document.getElementById("Monitor")) {
                 $.ajax({
                     url: 'api/DeviceReading/',
                     method: 'GET',
-                    success: function (data) {
-                        self.latestReadings = self.getLatestReadings(data);
-                        self.speedReadings = self.getSpeedReadings(data);
-                        self.drawSpeedTrendSvg();
+                    success: function(data) {
+                        self.latestReadings = data;
+
+                        if (!self.speedTrendSvg.value) {
+                            self.speedTrendSvg.value = self.getSpeedTrendSvg();
+                        } else {
+                            self.speedTrendSvg.value.update(data);
+                        }
+                        if (!self.legend.value) {
+                            self.legend.value = self.getLegend();
+                        }
+                        if (!self.totalBoardSvg.value) {
+                            self.totalBoardSvg.value = self.getTotalBoardBarChartSvg();
+                        } else {
+                            self.totalBoardSvg.value.update(data);
+                        }
                     },
-                    error: function (error) {
+                    error: function(error) {
                         console.log(error);
                     }
                 });
             },
-            getLatestReadings(data) {
-                return data.map(val => {
-                    var lastReading = val.readings[val.readings.length - 1];
-                    var reducer = (accumulator, currentValue) => accumulator + currentValue;
-                    return {
-                        speed: lastReading.speed,
-                        dateTime: lastReading.dateTime,
-                        packageTrackingAlarmState: lastReading.packageTrackingAlarmState,
-                        totalBoards: val.readings.map(r => r.currentBoards).reduce(reducer),
-                        recipeCount: val.readings.map(r => r.currentRecipeCount).reduce(reducer)
+            updateDictForSpeedTrendSvg(data) {
+                for (var i = 0; i < data.length; i++) {
+                    var current = data[i];
+                    var reading = {
+                        dateTime: current.dateTime,
+                        speed: current.speed
                     };
-                });
+                    var dataAssociatedWithCurrentId = this.speedTrendSvg.dict[current.id];
+                    if (!dataAssociatedWithCurrentId) {
+                        this.speedTrendSvg.dict[current.id] = {
+                            readings: [reading]
+                        };
+                    } else {
+                        dataAssociatedWithCurrentId.readings.push(reading);
+                    }
+                }
             },
-            getSpeedReadings(data) {
-                return data.map(val => {
-                    return {
-                        id: val.id,
-                        values: val.readings.map(r => {
-                            return { dateTime: new Date(r.dateTime), speed: +r.speed };
-                        })
-                    };
-                });
-            },
-            drawSpeedTrendSvg() {
-                var trendSvg = d3.select("#Trend_svg"),
-                    margin = { top: 20, right: 80, bottom: 30, left: 50 },
-                    width = trendSvg.attr("width") - margin.left - margin.right,
-                    height = trendSvg.attr("height") - margin.top - margin.bottom,
-                    g = trendSvg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            getSpeedTrendSvg() {
 
-                var x = d3.scaleTime().range([0, width]),
-                    y = d3.scaleLinear().range([height, 0]),
-                    z = d3.scaleOrdinal(d3.schemeCategory10);
+                var svgSettings = this.getSvgSettingsByTarget("#Trend_svg"),
+                    maximumDataLength = 30,
+                    transitionSettings = this.transitionSettings,
+                    timeBeforeDuration = new Date(Date.now() - transitionSettings.duration);
 
-                var line = d3.line()
-                    .curve(d3.curveBasis)
-                    .x(function (d) { return x(d.dateTime); })
-                    .y(function (d) { return y(d.speed); });
+                var xScale = d3.scaleTime()
+                    .range([0, svgSettings.width])
+                    .domain([timeBeforeDuration - (maximumDataLength - 2), timeBeforeDuration - transitionSettings.duration]);
 
-                x.domain(d3.extent(this.speedReadings[0].values, function(v) {
-                    return v.dateTime;
-                }));
+                var xAxis = d3.axisBottom(xScale);
 
-                y.domain([
-                    d3.min(this.speedReadings, function (c) { return d3.min(c.values, function (d) { return d.speed; }); }),
-                    d3.max(this.speedReadings, function (c) { return d3.max(c.values, function (d) { return d.speed; }); })
-                ]);
+                var xAxisSection = svgSettings.container.append("g")
+                    .attr("class", "x axis")
+                    .attr("transform", "translate(0," + svgSettings.height + ")")
+                    .call(xAxis);
 
-                z.domain(this.speedReadings.map(function (d) { return d.id; }));
+                var yScale = d3.scaleLinear()
+                    .range([svgSettings.height, 0])
+                    .domain([0, 100]);
 
-                g.append("g")
-                    .attr("class", "axis axis--x")
-                    .attr("transform", "translate(0," + height + ")")
-                    .call(d3.axisBottom(x));
-
-                g.append("g")
-                    .attr("class", "axis axis--y")
-                    .call(d3.axisLeft(y))
+                svgSettings.container.append("g")
+                    .attr("class", "y axis")
+                    .call(d3.axisLeft(yScale))
                     .append("text")
                     .attr("transform", "rotate(-90)")
                     .attr("y", 6)
@@ -95,24 +101,215 @@ if (document.getElementById("Monitor")) {
                     .attr("fill", "#000")
                     .text("Conveyor Speed");
 
-                var deviceReading = g.selectAll(".deviceReading")
-                    .data(this.speedReadings)
-                    .enter().append("g")
-                    .attr("class", "deviceReading");
+                var line = d3.line()
+                    .curve(d3.curveBasis)
+                    .x(function (d) { return xScale(new Date(d.dateTime)); })
+                    .y(function (d) { return yScale(d.speed); });
 
-                deviceReading.append("path")
-                    .attr("class", "line")
-                    .attr("d", function (d) { return line(d.values); })
-                    .style("stroke", function (d) { return z(d.id); });
+                var paths = svgSettings.container
+                    .append("svg")
+                    .append("g");
 
-                deviceReading.append("text")
-                    .datum(function (d) { return { id: d.id, value: d.values[d.values.length - 1] }; })
-                    .attr("transform",
-                        function (d) { return "translate(" + x(d.value.dateTime) + "," + y(d.value.speed) + ")"; })
-                    .attr("x", 3)
-                    .attr("dy", "0.35em")
-                    .style("font", "10px sans-serif")
+                var svgDict = this.speedTrendSvg.dict;
+                var self = this;
+                var lineChart = {};
+                lineChart.update = (data) => {
+
+                    self.updateDictForSpeedTrendSvg(data);
+
+                    var now = new Date();
+                    var colorFactor = 1;
+
+                    for (var name in svgDict) {
+                        var device = svgDict[name];
+                        if (!device.path) {
+                            var path = paths
+                                .append('path')
+                                .data(device.readings)
+                                .attr('class', 'line')
+                                .style('stroke', this.getColorByFactor(colorFactor++));
+                            svgDict[name] = Object.assign(svgDict[name], { path: path });
+                        }
+                        device.path
+                            .datum(device.readings)
+                            .attr('d', line);
+
+                        if (device.readings.length > maximumDataLength) {
+                            device.readings.shift();
+                        }
+                    }
+
+                    //// Shift domain
+                    xScale.domain([
+                        now - (maximumDataLength - 2) * transitionSettings.duration,
+                        now - transitionSettings.duration
+                    ]);
+
+                    // Slide x-axis left
+                    xAxisSection
+                        .transition()
+                        .duration(transitionSettings.duration)
+                        .ease(transitionSettings.ease)
+                        .call(xAxis);
+
+                    //// Slide paths left
+                    paths.attr('transform', null)
+                        .transition()
+                        .duration(transitionSettings.duration)
+                        .ease(transitionSettings.ease)
+                        .attr('transform', 'translate(' + xScale(now - (maximumDataLength - 1) * transitionSettings.duration) + ')');
+                };
+
+                return lineChart;
+            },
+            getTotalBoardBarChartSvg() {
+
+                var target = "#Bar_svg";
+                var dataset = this.latestReadings.map((d) => ({
+                    id: d.id,
+                    boards: d.totalBoards
+                }));
+
+                var svgSettings = this.getSvgSettingsByTarget(target);
+                var barSettings = {
+                    minimumPadding: 50,
+                    maximumWidth: 50
+                };
+                barSettings = Object.assign(barSettings,
+                {
+                    padding: d3.min([svgSettings.width / (dataset.length * 2), barSettings.minimumPadding]),
+                    width: d3.min([svgSettings.width / dataset.length - barSettings.padding, barSettings.maximumWidth])
+                });
+
+                // x axis
+                var xScale = d3.scaleBand()
+                    .domain(dataset.map((d) => d.id))
+                    .range([0, svgSettings.width])
+                    .padding(barSettings.padding);
+
+                var xAxis = d3.axisBottom()
+                    .scale(xScale);
+
+                svgSettings.container.append("g")
+                    .attr("class", "x axis")
+                    .attr("transform", "translate(0," + svgSettings.height + ")")
+                    .call(xAxis)
+                    .append("text")
+                    .attr("dx", "-1.5em")
+                    .attr("color", "white")
+                    .attr("x", svgSettings.width)
+                    .attr("y", -5)
+                    .text("Device");
+
+                // y axis
+                var yScale = d3.scaleLinear()
+                    .domain([0, d3.max(dataset, (d) => d.boards)])
+                    .range([svgSettings.height, 0]);
+                
+                var yAxis = d3.axisLeft()
+                    .scale(yScale);
+
+                svgSettings.container.append("g")
+                    .attr("class", "y axis")
+                    .call(yAxis)
+                    .append("text")
+                    .attr("transform", "rotate(-90)")
+                    .attr("y", 6)
+                    .attr("dy", "0.71em")
+                    .attr("fill", "#000")
+                    .text("Total Boards");
+                
+                // bar creation
+                var rects = svgSettings.container.selectAll("rect")
+                    .data(dataset)
+                    .enter()
+                    .append("rect")
+                    .attr("class", "bar")
+                    .attr("x", (d) => xScale(d.id) - barSettings.width / 2)
+                    .attr("y", (d) => yScale(d.boards))
+                    .attr("width", barSettings.width)
+                    .attr("height", (d) => svgSettings.height - yScale(d.boards))
+                    .attr("fill", (d, i) => this.getColorByFactor(i + 1));
+
+                // text label
+                var texts = svgSettings.container.append("g")
+                    .selectAll("text")
+                    .data(dataset)
+                    .enter()
+                    .append("text")
+                    .text((d) => d.boards)
+                    .attr("x", (d => xScale(d.id)))
+                    .attr("y", (d) => yScale(d.boards) - 5)
+                    .attr("font-family", "sans-serif")
+                    .attr("text-anchor", "middle");
+
+                var barChart = {};
+                var transitionSettings = this.transitionSettings;
+                barChart.update = (data) => {
+
+                    var newDataSet = data.map((d) => ({
+                        id: d.id,
+                        boards: d.totalBoards
+                    }));
+
+                    yScale.domain([0, d3.max(newDataSet, (d) => d.boards)]);
+
+                    rects.data(newDataSet)
+                        .transition()
+                        .duration(transitionSettings.duration)
+                        .ease(transitionSettings.ease)
+                        .attr("y", function(d) { return yScale(d.boards); })
+                        .attr("height", (d) => svgSettings.height - yScale(d.boards));
+
+                    texts.data(newDataSet)
+                        .text((d) => d.boards)
+                        .attr("y", (d) => yScale(d.boards) - 5);            
+                };
+
+                return barChart;
+            },
+            getLegend() {
+                var table = d3.select("#legend")
+                    .append("table")
+                    .attr('class', 'trend legend');
+
+                var trs = table
+                    .append("tbody").selectAll("tr")
+                    .data(this.latestReadings)
+                    .enter()
+                    .append("tr");
+
+                trs.append("td").append("svg")
+                    .attr("width", '16')
+                    .attr("height", '16')
+                    .append("rect")
+                    .attr("width", '16')
+                    .attr("height", '16')
+                    .attr("fill", (d, i) => this.getColorByFactor(i + 1));
+
+                trs.append("td")
                     .text(function (d) { return d.id; });
+
+                return table;
+            },
+			getSvgSettingsByTarget(target) {
+
+                var svg = d3.select(target),
+                    margin = { top: 20, right: 80, bottom: 30, left: 25 },
+                    width = 480 - margin.left - margin.right,
+                    height = 500 - margin.top - margin.bottom,
+                    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+                return {
+                    svg: svg,
+                    margin: margin,
+                    width: width,
+                    height: height,
+                    container: g
+                };
+            },
+            getColorByFactor(factor) {
+                return "rgb(" + (factor * 520) % 255 + ", " + (factor * 320) % 255 + ", " + (factor * 720) % 255 + ")";
             }
         }
     });
