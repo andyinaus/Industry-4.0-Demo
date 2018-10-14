00749e2c967cdc9209db1dca8b36987e959993b8
AndyHsu <andy.hsu.job@gmail.com>
2018-10-14 10:41:39 +0800

    Renamed to LatestDeviceReadingModel.cs

---
+++ IoTPlatform/Models/LatestDeviceReadingModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IoTPlatform.Models
+{
+    public class LatestDeviceReadingModel
+    {
+        public string Id { get; set; }
+
+        public string DeviceType { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public int Speed { get; set; }
+
+        public string PackageTrackingAlarmState { get; set; }
+
+        public long TotalBoards { get; set; }
+
+        public long TotalRecipeCount { get; set; }
+    }
+}
