e025e570f28fae33ef660a0fb45fbc8b3db8141f
AndyHsu <andy.hsu.job@gmail.com>
2018-10-15 19:45:20 +0800

    Added vue js into build pipeline

--- IoTPlatform/gulpfile.js
+++ IoTPlatform/gulpfile.js
@@ -1,4 +1,5 @@
-﻿"use strict";
+﻿/// <binding BeforeBuild='build:dev' />
+"use strict";
 
 var gulp = require("gulp"),
     rimraf = require("rimraf"),
@@ -14,6 +15,8 @@ var paths = {
 
 paths.js = paths.assets + "js/*.js";
 paths.bootstrapJs = paths.node_modules + 'bootstrap/dist/js/bootstrap.js';
+paths.vueJs = paths.node_modules + 'vue/dist/vue.js';
+paths.jquery = paths.node_modules + 'jquery/dist/jquery.js';
 paths.jquery = paths.node_modules + 'jquery/dist/jquery.js';
 paths.minJs = paths.webroot + "js/**/*.min.js";
 paths.css = paths.assets + "css/*.css";
@@ -35,7 +38,7 @@ gulp.task("clean:css", function (cb) {
 gulp.task('clean', ['clean:js', 'clean:css']);
 
 gulp.task("dev:js", function () {
-    return gulp.src([paths.js, paths.bootstrapJs, paths.jquery, "!" + paths.minJs], { base: "." })
+    return gulp.src([paths.js, paths.bootstrapJs, paths.vueJs, paths.jquery, "!" + paths.minJs], { base: "." })
         .pipe(concat(paths.concatJsDest))
         .pipe(gulp.dest("."));
 });
@@ -49,7 +52,7 @@ gulp.task("dev:css", function () {
 gulp.task('dev', ['dev:js', 'dev:css']);
 
 gulp.task("min:js", function () {
-    return gulp.src([paths.js, paths.bootstrapJs, paths.jquery, "!" + paths.minJs], { base: "." })
+    return gulp.src([paths.js, paths.bootstrapJs, paths.vueJs, paths.jquery, "!" + paths.minJs], { base: "." })
         .pipe(concat(paths.concatMinJsDest))
         .pipe(uglify())
         .pipe(gulp.dest("."));
