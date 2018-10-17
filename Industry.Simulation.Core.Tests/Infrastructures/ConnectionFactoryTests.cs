16a825252758a2eee66f300f8a9c9c75044893f4
AndyHsu <andy.hsu.job@gmail.com>
2018-10-17 15:04:20 +0800

    Added Core test project, and added ConnectionFactoryTests.cs to it

---
+++ Industry.Simulation.Core.Tests/Infrastructures/ConnectionFactoryTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using Industry.Simulation.Core.Infrastructures;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Industry.Simulation.Core.Tests.Infrastructures
+{
+    [TestClass]
+    public class ConnectionFactoryTests
+    {
+        private const string ValidConnectionString = "Server=.;Integrated Security=true;";
+
+        [TestMethod]
+        public void CtorWhenConfigurationIsNullShouldThrowArgumentNullException()
+        {
+            Action target = () => new ConnectionFactory(null);
+
+            Assert.ThrowsException<ArgumentNullException>(target);
+        }
+
+        [TestMethod]
+        public void CreateConnectionAsyncWhenConfigurationIsValidShouldReturnNewOpenConnection()
+        {
+            var mockConfig = new Mock<IConfiguration>();
+            mockConfig.Setup(m => m.GetSection("ConnectionStrings")[It.IsAny<string>()])
+                .Returns(ValidConnectionString);
+
+            var factory = new ConnectionFactory(mockConfig.Object);
+
+            var conn = factory.CreateConnectionAsync().Result;
+
+            Assert.AreEqual(ConnectionState.Open, conn.State);
+            Assert.AreNotSame(conn, factory.CreateConnectionAsync().Result);
+        }
+    }
+}
