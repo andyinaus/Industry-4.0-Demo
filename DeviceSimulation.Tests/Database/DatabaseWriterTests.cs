8965262c34cc27b50ebdfcf30aaafeee1f69c826
AndyHsu <andy.hsu.job@gmail.com>
2018-10-17 15:03:48 +0800

    Added DatabaseHelper.cs and DatabaseWriterTests.cs

---
+++ DeviceSimulation.Tests/Database/DatabaseWriterTests.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using DeviceSimulation.Database;
+using DeviceSimulation.Simulation;
+using DeviceSimulation.Tests.TestHelpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Serilog;
+
+namespace DeviceSimulation.Tests.Database
+{
+    [TestClass]
+    public class DatabaseWriterTests
+    {
+        [TestMethod]
+        public void CtorWhenConnectionFactoryIsNullShouldThrowArgumentNullException()
+        {
+            Action target = () => new DatabaseWriter(null, Mock.Of<ILogger>());
+
+            Assert.ThrowsException<ArgumentNullException>(target);
+        }
+
+        [TestMethod]
+        public void CtorWhenLoggerIsNullShouldThrowArgumentNullException()
+        {
+            Action target = () => new DatabaseWriter(DatabaseHelper.GetMockConnectionFactory(), null);
+
+            Assert.ThrowsException<ArgumentNullException>(target);
+        }
+
+        [TestMethod]
+        public void WriteAsyncWhenResultIsNullShouldThrowArgumentNullException()
+        {
+            var writer = new DatabaseWriter(DatabaseHelper.GetMockConnectionFactory(), Mock.Of<ILogger>());
+
+            Func<Task> target = () => writer.WriteAsync(null);
+
+            Assert.ThrowsExceptionAsync<ArgumentNullException>(target);
+        }
+
+        [TestMethod]
+        public void WriteAsyncWhenResultIsValidShouldWriteToDatabase()
+        {
+            var factory = DatabaseHelper.GetMockConnectionFactory();
+            using (var connection = factory.CreateConnectionAsync().Result)
+            {
+                DatabaseHelper.CreateTablesIfNotExist();
+                var deviceId = AddDummyDevviceToInMemoryDatabase();
+
+                var result = new ConveyorSimulator.SimulationResult
+                {
+                    Id = deviceId,
+                    Speed = 213,
+                    DateTime = DateTime.UtcNow,
+                    PackageTrackingAlarmState = PackageTrackingAlarmState.Okay,
+                    CurrentRecipeCount = 2,
+                    CurrentBoards = 3
+                };
+
+                var writer = new DatabaseWriter(factory, Mock.Of<ILogger>());
+
+                writer.WriteAsync(result).Wait();
+
+                var results = connection.Query("SELECT * FROM DeviceReadings");
+
+                Assert.AreEqual(1, results.Count());
+            }
+        }
+
+        private static string AddDummyDevviceToInMemoryDatabase()
+        {
+            var device = new
+            {
+                Id = "33333",
+                Type = "Dummy"
+            };
+
+            using (var connection = DatabaseHelper.GetMockConnectionFactory().CreateConnectionAsync().Result)
+            {
+                connection.Execute("INSERT INTO Devices VALUES(@Id, @Type)", device);
+            }
+
+            return device.Id;
+        }
+    }
+}
---
+++ DeviceSimulation.Tests/TestHelpers/DatabaseHelper.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
+using Industry.Simulation.Core.Infrastructures;
+using Moq;
+
+namespace DeviceSimulation.Tests.TestHelpers
+{
+    public class DatabaseHelper
+    {
+        private const string CreateDevicesScript = "CREATE TABLE Devices ([ID] varchar(36) NOT NULL PRIMARY KEY, [Type] varchar(100) NOT NULL)";
+        private const string CreateDeviceReadingsScript = "CREATE TABLE DeviceReadings ([DeviceID] varchar(36) NOT NULL," +
+            " [DateTime] DateTime NOT NULL, [Speed] INT NOT NULL, [PackageTrackingAlarmState] varchar(10) NOT NULL," +
+            " [CurrentBoards] INT NOT NULL, [CurrentRecipeCount] INT NOT NULL, PRIMARY KEY([DeviceID], [DateTime]), FOREIGN KEY([DeviceID]) REFERENCES Devices(ID))";
+
+        private const string InMemoryConnectionString = "Filename=Test; Mode=Memory; Cache=Shared";
+
+        public static IConnectionFactory GetMockConnectionFactory()
+        {
+            var mockFactory = new Mock<IConnectionFactory>();
+
+            mockFactory.Setup(m => m.CreateConnectionAsync())
+                .ReturnsAsync(() =>
+                {
+                    var connection = new SqliteConnection(InMemoryConnectionString);
+
+                    connection.Open();
+
+                    return connection;
+                });
+
+            return mockFactory.Object;
+        }
+
+        public static void CreateTablesIfNotExist()
+        {
+            var factory = GetMockConnectionFactory();
+
+            using (var connection = factory.CreateConnectionAsync().Result)
+            {
+                connection.Execute(CreateDevicesScript);
+                connection.Execute(CreateDeviceReadingsScript);
+            }
+        }
+    }
+}
